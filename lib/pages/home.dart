4bef40bf92d4fdf026212f392137ff84b376f286
Jago (C. Santiago Melon) <jago@kairosds.com>
2021-02-22 21:24:20 +0100

    Google sign in working

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final googleSignIn = GoogleSignIn();
 
 class Home extends StatefulWidget {
   @override
@@ -7,8 +10,92 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _isAuth = false;
+
+  @override
+  void initState() {
+    super.initState();
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      handleSignin(account);
+    }, onError: (err) {
+      print('Error signin in: $err');
+    });
+    googleSignIn.signInSilently(suppressErrors: false).then((account) {
+      handleSignin(account);
+    }).catchError((err) {
+      print('Error signin in: $err');
+    });
+  }
+
+  handleSignin(GoogleSignInAccount account) {
+    if (account != null) {
+      print('User signed in: $account');
+      setState(() {
+        _isAuth = true;
+      });
+    } else {
+      setState(() {
+        _isAuth = false;
+      });
+    }
+  }
+
+  login() {
+    googleSignIn.signIn();
+  }
+
+  logout() {
+    googleSignIn.signOut();
+  }
+
+  Widget buildAuthScreen() {
+    return RaisedButton(child: Text('Log out'), onPressed: logout);
+  }
+
+  Widget buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+              Theme.of(context).accentColor.withOpacity(0.8),
+              Theme.of(context).primaryColor,
+            ])),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Fluttershare',
+              style: TextStyle(
+                fontFamily: 'Signatra',
+                fontSize: 90.0,
+                color: Colors.white,
+              ),
+            ),
+            GestureDetector(
+              onTap: login,
+              child: Container(
+                width: 260.0,
+                height: 60.0,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(
+                            'assets/images/google_signin_button.png'),
+                        fit: BoxFit.cover)),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Home");
+    return _isAuth ? buildAuthScreen() : buildUnAuthScreen();
   }
 }
